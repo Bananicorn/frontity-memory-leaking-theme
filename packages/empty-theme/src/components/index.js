641ec0d29d041b5a548481d795a88089216fe17f
Georg Ratschiller <georg.ratschiller@wunderfarm.com>
2022-02-16 14:47:50 +0100

    trimmed down the theme some more

--- packages/empty-theme/src/components/index.js
+++ packages/empty-theme/src/components/index.js
@@ -1,13 +1,8 @@
 import { connect } from "frontity";
-import { useEffect } from "react";
 
 const Theme = ({ state, actions, libraries }) => {
 	const data = state.source.get(state.router.link);
 	const post = state.source[data.type][data.id];
-
-	useEffect(() => {
-		actions.source.fetch("/");
-	}, [actions.source]);
 
 	return data.isReady ? (
 		<>
--- packages/empty-theme/src/index.js
+++ packages/empty-theme/src/index.js
@@ -2,18 +2,8 @@ import Theme from "./components";
 
 const EmptyTheme = {
 	name: "empty-theme",
-	state: {
-		theme: {
-			autoPrefetch: "in-view",
-		}
-	},
 	roots: {
 		theme: Theme,
-	},
-	actions: {
-		//beforeSSR: async ({ state, actions }) => {
-			//await actions.source.fetch(state.router.link);
-		//}
 	},
 };
 
